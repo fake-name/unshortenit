a911239688241dc878410a19fb4a04e29649d399
Fake-Name <something@fake-url.com>
2018-01-04 23:16:20 -0800

    Fix https://github.com/jkehler/unshortenit/issues/20

--- setup.py
+++ setup.py
@@ -3,7 +3,19 @@
 
 import os
 import sys
-import unshortenit
+
+# This is horrible.
+# On the other hand, it's either this, or parse
+# the file with a regex, since we can't import it until
+# the dependencies are installed.
+with open("unshortenit/__init__.py") as fp:
+    lines = fp.readlines()
+
+ver_lines = [tmp for tmp in lines if "__version__" in tmp]
+assert ver_lines, "Could not find a version value in 'unshortenit/__init__.py'!"
+for line in ver_lines:
+    exec(line)
+
 
 try:
     from setuptools import setup
@@ -19,7 +31,7 @@ history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
     name='unshortenit',
-    version=unshortenit.__version__,
+    version=__version__,
     description='Unshortens adf.ly, sh.st, and any 301 redirected shortener urls',
     long_description=readme + '\n\n' + history,
     author='Jeff Kehler',
@@ -30,8 +42,7 @@ setup(
     ],
     package_dir={'unshortenit': 'unshortenit'},
     include_package_data=True,
-    install_requires=['requests'
-    ],
+    install_requires=['requests'],
     license="MIT",
     zip_safe=False,
     keywords='unshortener adf.ly lnx.lu sh.st shortener',
